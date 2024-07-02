1fb5767c63eec03ef28c94cbddaa02f860ee6960
Nadhifazahra <nadhifazahra37@gmail.com>
2024-07-02 22:59:24 +0700

    first commit

---
+++ app/Http/Controllers/API/ListingController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\Listing;
+
+class ListingController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $listing = Listing::withCount('transaction')->orderBy('transaction_count', 'desc')->paginate();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Get all listings',
+            'data' => $listing
+        ]);
+    }
+
+
+    public function show(Listing $listing): JsonResponse
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Get detail listings',
+            'data' => $listing
+        ]);
+    }
+
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Listing;
+use App\Models\Transaction;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Doctrine\DBAL\Driver\OCI8\Exception\SequenceDoesNotExist;
+use GuzzleHttp\Promise\Create;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@nginapyuk.test',
+            'role' => 'admin'
+        ]);
+        
+        $users = User::factory(10)->create();
+        $listings = Listing::factory(10)->create();
+
+        Transaction::factory(10)
+        ->state(
+            new Sequence(
+                fn(Sequence $sequence) => [
+                    'user_id' => $users->random(),
+                    'listing_id' => $listings->random(),
+                ],
+            )
+        )->create();
+    }
+}
